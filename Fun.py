48520ee7fa12e9c1b8fb5fbc8ea859f9214d1cd0
Bakhshial <bakhshil559@gmail.com>
2023-04-19 16:08:25 +0500

    Add nice to meet you

---
+++ Fun.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 19 15:42:47 2023
+
+@author: HACKER CODER
+"""
+
+def add(a,b,d):
+    c=a+b+d
+    return c
+print(add(1,2,3))cl
+print("hello preen")
